a5cca2c95c849daa62204cc6ea25d298a4a70cf7
AlexPy1 <77441740+AlexPy1@users.noreply.github.com>
2022-06-07 00:24:17 -0700

    Lesson 5 (#4)
    * lesson_5 ready

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,9 +1,4 @@
-
 from django.shortcuts import get_object_or_404
-
-from datetime import datetime
-
-
 from django.views.generic import TemplateView
 
 from mainapp import models as mainapp_models
@@ -20,7 +15,6 @@ class NewsPageView(TemplateView):
         # Get all previous data
         context = super().get_context_data(**kwargs)
         # Create your own data
-
         context["news_qs"] = mainapp_models.News.objects.all()[:5]
         return context
 
@@ -31,19 +25,6 @@ class NewsPageDetailView(TemplateView):
     def get_context_data(self, pk=None, **kwargs):
         context = super().get_context_data(pk=pk, **kwargs)
         context["news_object"] = get_object_or_404(mainapp_models.News, pk=pk)
-
-        context["news_title"] = "Громкий новостной заголовок"
-        context["news_preview"] = "Предварительное описание, которое заинтересует каждого"
-        context["range"] = range(5)
-        context["datetime_obj"] = datetime.now()
-        return context
-
-
-class NewsWithPaginatorView(NewsPageView):
-    def get_context_data(self, page, **kwargs):
-        context = super().get_context_data(page=page, **kwargs)
-        context["page_num"] = page
-
         return context
 
 
@@ -73,7 +54,3 @@ class ContactsPageView(TemplateView):
 
 class DocSitePageView(TemplateView):
     template_name = "mainapp/doc_site.html"
-
-
-class LoginPageView(TemplateView):
-    template_name = "mainapp/login.html"
